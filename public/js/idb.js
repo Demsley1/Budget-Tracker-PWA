914b8faf1afa3d631e5761908d3231f96f040f06
Denva <emsley3684@gmail.com>
2022-02-13 11:50:46 -0500

    add indexed db connection file, ad set up reuest handler and event listener to hold data in idb

---
+++ public/js/idb.js
@@ -0,0 +1,9 @@
+let db;
+const request = indexedDB.open('budget-tracker', 1)
+
+request.onupgradeneeded = function(event) {
+    // set the db reference to the passed in event
+    const db = event.target.result;
+    // Object Store that acts like a SQL table, name of objectStore is "expense" and has a self incrementing primary key.
+    db.createObjectStore('expense', { autoIncrement: true });
+}
