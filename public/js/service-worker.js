9c8ff6e902801585628389fa391ef73f04d64e15
Denva <emsley3684@gmail.com>
2022-02-16 16:36:47 -0500

    named service worker cache and data chache, added files to cache to service worker file and created service worker.js file

---
+++ public/js/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'budget-tracker-cache-v1';
+const DATA_CACHE_NAME = 'budget-data-cache-v1';
+
+const FILES_TO_CACHE = [
+    '../index.html',
+    '../css/style.css',
+    '../js/idb.js',
+    '../js/index.js',
+    '../icons/icon-72x72.png',
+    '../icons/icon-96x96.png',
+    '../icons/icon-128x128.png',
+    '../icons/icon-144x144.png',
+    '../icons/icon-152x152.png',
+    '../icons/icon-192x192.png',
+    '../icons/icon-384x384.png',
+    '../icons/icon-512x512.png'
+]
+
+
+
+self.addEventListener('install', function(e) {
+
+})
