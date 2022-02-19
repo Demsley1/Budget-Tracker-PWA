a0b3751d9f6aad4af9a84203f718ec3adae7b072
Denva <emsley3684@gmail.com>
2022-02-19 13:45:59 -0500

    moved service-worker to root of public directory

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,19 +3,19 @@ const DATA_CACHE_NAME = 'budget-data-cache-v1';
 
 const FILES_TO_CACHE = [
     '/',
-    '../index.html',
-    '../css/styles.css',
+    '/index.html',
+    '/css/styles.css',
     '/manifest.json',
-    '../js/idb.js',
-    '../js/index.js',
-    '../icons/icon-72x72.png',
-    '../icons/icon-96x96.png',
-    '../icons/icon-128x128.png',
-    '../icons/icon-144x144.png',
-    '../icons/icon-152x152.png',
-    '../icons/icon-192x192.png',
-    '../icons/icon-384x384.png',
-    '../icons/icon-512x512.png'
+    '/js/idb.js',
+    '/js/index.js',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
@@ -68,10 +68,19 @@ self.addEventListener('fetch', (e) => {
     }
 
     e.respondWith(
-        caches.match(e.request).then(request => {
+        fetch(e.request).catch(() => {
+            return caches.match(e.request).then(response => {
+                if(response){
+                    return response;
+                } else if(e.request.headers.get('accept').includes('text/html')){
+                    return caches.match('/');
+                }
+            })
+        })
+        /*caches.match(e.request).then(request => {
             if(request){
                 return request || fetch(e.request) 
             }
-        })
+        })*/
     )
 })
