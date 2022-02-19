50a1686aae71b5d5981bb126822a13f8bcb4b082
Denva <emsley3684@gmail.com>
2022-02-19 14:42:41 -0500

    added comments, cleaned-up service worker code

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,28 +1,35 @@
+// create variable for database, and set a request variable that opens index db with custom name, and at the very least uses position 1.
 let db;
 const request = indexedDB.open('budget-tracker', 1)
 
 request.onupgradeneeded = function(event) {
-    // set the db reference to the passed in event
+    // set the db reference to the passed in events result
     const db = event.target.result;
     // Object Store that acts like a SQL table, name of objectStore is "expense" and has a self incrementing primary key.
     db.createObjectStore('expense', { autoIncrement: true });
 }
 
+// run the funciton to send stored data to server if nvigator shows that it is back online.
 request.onsuccess = function(event){
     db = event.target.result;
 
     if(navigator.onLine){uploadData()}
 };
 
+// error code logger
 request.onerror = function(event){
     console.log(event.target.errorCode)
 };
 
+
 function saveRecord(data){
+    // set up a new transaction for the database with read and write priviliges
     const transaction = db.transaction(['expense'], 'readwrite');
 
+    // deposit the values in to be stored as an object 
     const depositObjectStore = transaction.objectStore('expense');
 
+    // add the passed in values from the data parameters to created object store
     depositObjectStore.add(data);
 }
 
@@ -31,8 +38,10 @@ function uploadData(){
 
     const depositObjectStore = transaction.objectStore('expense');
 
+    // Get and read the data in the selected indexed db database
     const getAll = depositObjectStore.getAll();
 
+    // on success create a fetch request to a backend route for the server that will submit all data with a post request
     getAll.onsuccess = function(){
         if(getAll.result.length > 0){
             fetch('/api/transaction/bulk', {
@@ -48,6 +57,7 @@ function uploadData(){
                     throw new Error(serverResponse);
                 }
 
+                // once information is sent to server and is stored in the mongodb database, clear the indexed db database that is used for app.
                 const transaction = db.transaction(['expense'], 'readwrite');
                 const depositObjectStore = transaction.objectStore('expense')
                 depositObjectStore.clear();
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,8 @@
+// set up a chache_name and data_cache_name for app files to be saved under in browser cache
 const CACHE_NAME = 'budget-tracker-cache-v1';
 const DATA_CACHE_NAME = 'budget-data-cache-v1';
 
+// Select files that need to be cached to server for service worker to work, and their appropriate directories.
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
@@ -21,19 +23,24 @@ const FILES_TO_CACHE = [
 // Install the service worker
 self.addEventListener('install', function(e) {
     e.waitUntil(
+        // open up a new cache with the choosen cache name
         caches.open(CACHE_NAME).then(cache => {
             console.log("success");
+            // after installing the service worker add the files to cache array of files with the linked cache name.
             return cache.addAll(FILES_TO_CACHE);
         })
     );
+    // forces the waiting service-worker to become active
     self.skipWaiting();
 });
 
 // listen to activate event and clear cache storage if already populated
 self.addEventListener('activate', function(e) {
     e.waitUntil(
+        // opens up the cache storage existing in the browser when user logs into the site, and retrieves all caches and their names.
         caches.keys().then(keyList => {
             return Promise.all(
+                // map and sort all key in the array of stored caches, then will delete all that dont match the variable keys indicated in this script file.
                 keyList.map(key => {
                     if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                         console.log('removing old cache data: ', key);
@@ -43,24 +50,32 @@ self.addEventListener('activate', function(e) {
             );
         })
     );
+    // lets the newly installed service worker take control of all clients as soon as it is done installing.
     self.clients.claim();
 });
 
+// function to intercept fetch requests.
 self.addEventListener('fetch', (e) => {
     console.log('fetch request: '+ e.request.url)
+    // if request is going to a backend route then respond with...
     if(e.request.url.includes('/api/')){
         e.respondWith(
+            // open saved caches files that match the data cache name
             caches.open(DATA_CACHE_NAME)
+            // promise response that returns the fetch request
             .then(cache => {
                 return fetch(e.request)
                 .then(response => {
+                    // if response is ok then clone the response
                     if(response.status === 200){
                         cache.put(e.request.url, response.clone());
                     }
                     return response;
+                    // or log the error, and get whatever requested files that match from the cache 
                 }).catch(err => {
                     console.log("Network is Offline")
-                    return alert("User is offline right now!! Data is being saved to the local storage until you get back online, then it will be sent to the server.")
+                    alert("User is offline right now!! Data is being saved to the local storage until you get back online, then it will be sent to the server.")
+                    return cache.match(e.request);
                 })
             }).catch(err => console.log(err))
         );
@@ -68,6 +83,7 @@ self.addEventListener('fetch', (e) => {
     }
 
     e.respondWith(
+        // responds with successful request if not to the backend
         fetch(e.request).catch(() => {
             return caches.match(e.request).then(response => {
                 if(response){
@@ -77,10 +93,5 @@ self.addEventListener('fetch', (e) => {
                 }
             })
         })
-        /*caches.match(e.request).then(request => {
-            if(request){
-                return request || fetch(e.request) 
-            }
-        })*/
     )
 })
